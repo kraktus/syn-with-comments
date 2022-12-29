12283da9aabbca4041f2ae8d6c4dc542ad4947fe
kraktus <kraktus@users.noreply.github.com>
2022-12-29 18:25:32 +0100

    WIP switch to always use `byte_offset`

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -5,130 +5,102 @@ use syn::{visit_mut::VisitMut, ItemImpl};
 
 struct CommentsRetriever<'a> {
     input: &'a str,
-    // proc_macro raw `lo` and `hi` attributes are global, which mean if you use `parse_str` twice on the same str block
-    // you will have contiguous spans. eg: for an input of len 200. first one will be from 0-200, second 200-400.
-    // so we need to calcucate and offset between the span and the position in the input
-    offset: usize,
     ts: TokenStream,
 }
 
 impl<'a> CommentsRetriever<'a> {
-    fn new(input: &'a str) -> syn::Result<Option<Self>> {
+    fn new(input: &'a str) -> syn::Result<Self> {
         let ts: TokenStream = syn::parse_str(input)?;
-        if let Some(first_token) = ts.clone().into_iter().next() {
-            let offset_first_token_in_input = byte_offset(input, first_token.span().start());
-            // FIXME why minus 1 needed
-            let offset = dbg!(span_start(first_token.span())) - dbg!(offset_first_token_in_input) - 1;
-            Ok(Some(Self { input, offset, ts }))
+        Ok(Self { input, ts })
+    }
+
+    // return the byte offset bewteen `location` and the beginning and `input`
+    fn byte_offset(&self, location: LineColumn) -> usize {
+        let mut offset = 0;
+        for _ in 1..location.line {
+            offset += self.input[offset..].find('\n').unwrap() + 1;
+        }
+        offset
+            + self.input[offset..]
+                .chars()
+                .take(location.column)
+                .map(char::len_utf8)
+                .sum::<usize>()
+    }
+    fn span_start(&self, s: Span) -> usize {
+        self.byte_offset(s.start())
+    }
+
+    fn span_end(&self, s: Span) -> usize {
+        self.byte_offset(s.end())
+    }
+
+    fn comments_between(&self, end_last_span: usize, end: Span) -> TokenStream {
+        self.comments_between_raw(end_last_span, self.span_start(end))
+    }
+
+    fn comments_between_raw(&self, begin: usize, end: usize) -> TokenStream {
+        let mut between = self.input[begin..end + 1].trim();
+        if between.is_empty() {
+            TokenStream::new()
         } else {
-            Ok(None)
+            // FIXME why minus 1 needed?
+            between = &between[..between.len() - 1];
+            let comments = between
+                .split('\n')
+                .map(str::trim)
+                .filter(|s| !s.is_empty())
+                .map(|c| &c[2..]) // remove the `//` characters. TODO handle it with more care, inner/outer
+                .map(|comment| quote!(#[comment =  #comment]));
+            quote!(#(#comments)*)
         }
     }
-}
-// return the byte offset bewteen `location` and the beginning and `input`
-fn byte_offset(input: &str, location: LineColumn) -> usize {
-    let mut offset = 0;
-    for _ in 1..location.line {
-        offset += input[offset..].find('\n').unwrap() + 1;
-    }
-    offset
-        + input[offset..]
-            .chars()
-            .take(location.column)
-            .map(char::len_utf8)
-            .sum::<usize>()
-}
 
-// amazing if there is no better way
-// start is included, end is excluded
-fn span_start_and_end(s: Span) -> (usize, usize) {
-    let debug_span = format!("{s:?}"); // allocating a string every time...
-                                       // we know it's ASCII only
-                                       // because of the form: `bytes(start..stop)`
-    debug_span[6..debug_span.len() - 1]
-        .split_once("..")
-        .map(|(start, end)| {
-            (
-                usize::from_str(start).unwrap(),
-                usize::from_str(end).unwrap(),
-            )
-        })
-        .unwrap()
-}
-
-fn span_start(s: Span) -> usize {
-    span_start_and_end(s).0
-}
-
-fn span_end(s: Span) -> usize {
-    span_start_and_end(s).1
-}
-
-fn comments_between(input: &str, end_last_span: usize, end: Span) -> TokenStream {
-    comments_between_raw(input, end_last_span, span_start(end))
-}
-
-fn comments_between_raw(input: &str, begin: usize, end: usize) -> TokenStream {
-    let mut between = input[begin..end].trim();
-    if between.is_empty() {
-        TokenStream::new()
-    } else {
-        // FIXME why minus 1 needed?
-        between = &between[..between.len() -1];
-        let comments = between
-            .split('\n')
-            .map(str::trim)
-            .filter(|s| !s.is_empty())
-            .map(|c| &c[2..]) // remove the `//` characters. TODO handle it with more care, inner/outer
-            .map(|comment| quote!(#[comment =  #comment]));
-        quote!(#(#comments)*)
-    }
-}
-
-// TODO generalise input
-// TODO make this function not recursive
-fn handle_token_tree(input: &str, tt: TokenTree, end_last_span: usize) -> TokenStream {
-    match tt {
-        TokenTree::Group(group) => {
-            println!("In GROUP");
-            // comments before the group
-            let comments = comments_between(input, end_last_span, group.span());
-            let last_span_boundary = span_start(group.span()) + 1; // plus 1 to get over the brace/parenthesis/space
-            let inner_token_stream = if group.stream().is_empty() {
-                // if the group is empty, the only thing that can be inside is a comment
-                comments_between_raw(input, last_span_boundary, span_end(group.span()))
-            } else {
-                handle_token_stream(input, group.stream(), last_span_boundary)
-            };
-            let stream = quote!(#inner_token_stream);
-            let group_with_comments = Group::new(group.delimiter(), stream);
-            quote!(#comments #group_with_comments)
-        }
-        terminal_token => {
-            println!("In TERMINAL token");
-            let comments = comments_between(input, end_last_span, dbg!(&terminal_token).span());
-            quote!(#comments #terminal_token)
+    // TODO make this function not recursive
+    fn handle_token_tree(&self, tt: TokenTree, end_last_span: usize) -> TokenStream {
+        match tt {
+            TokenTree::Group(group) => {
+                println!("In GROUP");
+                // comments before the group
+                let comments = self.comments_between(end_last_span, group.span());
+                let last_span_boundary = self.span_start(group.span()) + 1; // plus 1 to get over the brace/parenthesis/space
+                let inner_token_stream = if group.stream().is_empty() {
+                    // if the group is empty, the only thing that can be inside is a comment
+                    self.comments_between_raw(last_span_boundary, self.span_end(group.span()))
+                } else {
+                    self.handle_token_stream(group.stream(), last_span_boundary)
+                };
+                let stream = quote!(#inner_token_stream);
+                let group_with_comments = Group::new(group.delimiter(), stream);
+                quote!(#comments #group_with_comments)
+            }
+            terminal_token => {
+                println!("In TERMINAL token");
+                let comments = self.comments_between(end_last_span, dbg!(&terminal_token).span());
+                quote!(#comments #terminal_token)
+            }
         }
     }
-}
 
-fn handle_token_stream(input: &str, ts: TokenStream, mut end_last_span: usize) -> TokenStream {
-    let inner_token_stream = ts.into_iter().map(|inner_tt| {
-        let inner_span = inner_tt.span();
-        let res = handle_token_tree(input, inner_tt, end_last_span);
-        println!("res {res}");
-        end_last_span = span_end(inner_span);
-        res
-    });
-    quote!(#(#inner_token_stream)*)
+    fn handle_token_stream(&self, ts: TokenStream, mut end_last_span: usize) -> TokenStream {
+        let inner_token_stream = ts.into_iter().map(|inner_tt| {
+            let inner_span = inner_tt.span();
+            let res = self.handle_token_tree(inner_tt, end_last_span);
+            println!("res {res}");
+            end_last_span = self.span_end(inner_span);
+            res
+        });
+        quote!(#(#inner_token_stream)*)
+    }
+
+    fn parse_str(self) -> TokenStream {
+        self.handle_token_stream(self.ts.clone(), 0)
+    }
 }
 
 fn parse_str(input: &str) -> syn::Result<TokenStream> {
     dbg!(input.len());
-    syn::parse_str(input).map(|ts: TokenStream| {
-        println!("{ts:?}");
-        handle_token_stream(input, quote!(#ts), 0)
-    })
+    CommentsRetriever::new(input).map(CommentsRetriever::parse_str)
 }
 
 fn main() {
