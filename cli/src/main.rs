0cada8d4d0026ca37433974f87566ac73eddc05c
kraktus <kraktus@users.noreply.github.com>
2022-12-29 15:11:38 +0100

    split `span_start_end` into `span_start` and `span_end`

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -5,7 +5,7 @@ use syn::{visit_mut::VisitMut, ItemImpl};
 
 // amazing if there is no better way
 // start is included, end is excluded
-fn span_start_end(s: Span) -> (usize, usize) {
+fn span_start_and_end(s: Span) -> (usize, usize) {
     let debug_span = format!("{s:?}"); // allocating a string every time...
                                        // we know it's ASCII only
                                        // because of the form: `bytes(start..stop)`
@@ -20,8 +20,16 @@ fn span_start_end(s: Span) -> (usize, usize) {
         .unwrap()
 }
 
+fn span_start(s: Span) -> usize {
+     span_start_and_end(s).0
+ }
+
+ fn span_end(s: Span) -> usize {
+     span_start_and_end(s).1
+ }
+
 fn comments_between(input: &str, end_last_span: usize, end: Span) -> TokenStream {
-    comments_between_raw(input, end_last_span, span_start_end(end).0)
+    comments_between_raw(input, end_last_span, span_start(end))
 }
 
 fn comments_between_raw(input: &str, begin: usize, end: usize) -> TokenStream {
@@ -49,10 +57,10 @@ fn handle_token_tree(input: &str, tt: TokenTree, end_last_span: usize) -> TokenS
             println!("In GROUP");
             // comments before the group
             let comments = comments_between(input, end_last_span, group.span());
-            let last_span_boundary = span_start_end(group.span()).0 + 1; // plus 1 to get over the brace/parenthesis/space
+            let last_span_boundary = span_start(group.span()) + 1; // plus 1 to get over the brace/parenthesis/space
             let inner_token_stream = if group.stream().is_empty() {
                 // if the group is empty, the only thing that can be inside is a comment
-                comments_between_raw(input, last_span_boundary, span_start_end(group.span()).1)
+                comments_between_raw(input, last_span_boundary, span_end(group.span()))
             } else {
                 handle_token_stream(input, group.stream(), last_span_boundary)
             };
@@ -61,7 +69,8 @@ fn handle_token_tree(input: &str, tt: TokenTree, end_last_span: usize) -> TokenS
             quote!(#comments #group_with_comments)
         }
         terminal_token => {
-            let comments = comments_between(input, end_last_span, terminal_token.span());
+            println!("In TERMINAL token");
+            let comments = comments_between(input, end_last_span, dbg!(&terminal_token).span());
             quote!(#comments #terminal_token)
         }
     }
@@ -72,28 +81,28 @@ fn handle_token_stream(input: &str, ts: TokenStream, mut end_last_span: usize) -
         let inner_span = inner_tt.span();
         let res = handle_token_tree(input, inner_tt, end_last_span);
         println!("res {res}");
-        end_last_span = span_start_end(inner_span).1;
+        end_last_span = span_end(inner_span);
         res
     });
     quote!(#(#inner_token_stream)*)
 }
 
 fn main() {
-    // let input = r#"
-    //     // comment on Thing
-    //     #[cfg(feature = foo)]
-    //     impl Thing {
-    //         // non-doc comment
-    //         fn f(&self) {// foo
-    //         }
-    //         // also comment
-    //         fn g(&self) {}
-    //     }
-    // "#;
-    let input = r#"(0) // foo
-        (// bar
-        )
+    let input = r#"
+        // comment on Thing
+        #[cfg(feature = foo)]
+        impl Thing {
+            // non-doc comment
+            fn f(&self) {// foo
+            }
+            // also comment
+            fn g(&self) {}
+        }
     "#;
+    // let input = r#"(0) // foo
+    //     (// bar
+    //     )
+    // "#;
 
     let impl_block: TokenStream = syn::parse_str(input).unwrap();
     println!("without comments debug: {:?}", quote!(#impl_block));
@@ -102,4 +111,15 @@ fn main() {
         "with comments: {}",
         handle_token_stream(&input, quote!(#impl_block), 0)
     );
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_name() {
+        
+    }
 }
